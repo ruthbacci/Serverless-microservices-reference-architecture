6bee01f6e3ad21cf18334c2ae5b94507c553d481
ruthbacci <ruth@ruthbacci.com>
2019-10-23 13:24:28 +0100

    Create settings.js

---
+++ web/serverless-microservices-web/public/js/settings.js
@@ -0,0 +1,13 @@
+// Auth
+window.authClientId = '8fae5277-395f-4f64-828e-eb5e64318635';
+window.authAuthority = 'https://rbrelecloudrideshare.b2clogin.com/tfp/rbrelecloudrideshare.onmicrosoft.com/B2C_1_default-signin/v2.0';
+window.authScopes = ['https://rbrelecloudrideshare.onmicrosoft.com/api/rbrideshare'];
+window.authEnabled = true;
+
+// API endpoints
+window.apiKey = 'ac769c32072e48cbbae826d8869d7227';
+window.apiBaseUrl = 'https://rbrideshare.azure-api.net';
+window.apiDriversBaseUrl = `${window.apiBaseUrl}/d`;
+window.apiTripsBaseUrl = `${window.apiBaseUrl}/t`;
+window.apiPassengersBaseUrl = `${window.apiBaseUrl}/p`;
+window.signalrInfoUrl = 'https://rbidesharetripsfunctionapp.azurewebsites.net/api/signalrinfo';
